53bb6ae858f5e6527c02a88c04e5027207a89e84
Luis Alberto Pena Nunez <luis.pena.nunez@gmail.com>
2022-12-22 09:01:10 -0700

    Added app icon

--- Rule30Exercise/ContentView.swift
+++ Rule30Exercise/ContentView.swift
@@ -10,10 +10,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Rule 30")
+                .font(.largeTitle)
+                .bold()
         }
         .padding()
     }
