815a258137f0438d9f9f1412ebaa5d8e6ee86d72
Luis Alberto Pena Nunez <luis.pena.nunez@gmail.com>
2022-12-22 08:52:54 -0700

    Initial Commit

---
+++ Rule30Exercise/Rule30ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rule30ExerciseApp.swift
+//  Rule30Exercise
+//
+//  Created by Luis Alberto Pena Nunez on 12/22/22.
+//
+
+import SwiftUI
+
+@main
+struct Rule30ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
